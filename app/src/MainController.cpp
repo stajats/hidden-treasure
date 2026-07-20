3ab682674e874ebbe76fe36a6b37ec98dee1e0c4
Tadija <tadijamatin0708@gmail.com>
2026-07-20 22:26:13 +0200

    Close App on ESC

--- app/src/MainController.cpp
+++ app/src/MainController.cpp
@@ -4,9 +4,19 @@
 
 #include "../include/MainController.hpp"
 
+#include "engine/platform/PlatformController.hpp"
 #include "spdlog/spdlog.h"
 
+#include <linux/input-event-codes.h>
+
 void app::MainController::initialize() {}
+bool app::MainController::loop() {
+    auto platform = engine::core::Controller::get<engine::platform::PlatformController>();
+    if (platform->key(engine::platform::KEY_ESCAPE).is_down()) {
+        return false;
+    }
+    return true;
+}
 std::string_view app::MainController::name() const {
     return "app::MainController";
 }
