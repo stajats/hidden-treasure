480fabd6dd4de3ac2d0d494dff52ac2ea47b2788
Tadija <tadijamatin0708@gmail.com>
2026-07-21 13:42:09 +0200

    Implement Phong lighting for boat.

--- app/resources/shaders/basic.glsl
+++ app/resources/shaders/basic.glsl
@@ -28,17 +28,35 @@ out vec4 FragColor;
 
 in vec2 TexCoords;
 in vec3 Normal;
+in vec3 FragPos;
+
+uniform vec3 materialAmbient;
+uniform vec3 materialSpecular;
+uniform float materialShininess;
+
+uniform vec3 viewPos;
 
 uniform sampler2D texture_diffuse1;
 uniform vec3 lightDir;
 
 void main() {
-    //FragColor = vec4(texture(texture_diffuse1, TexCoords).rgb, 1.0);
+    vec3 lightColor = vec3(1.0f, 0.8f, 0.8f);
+
+    //ambient
+    vec3 ambient = materialAmbient * texture(texture_diffuse1, TexCoords).rgb;
+
+    // diffuse
     vec3 norm = normalize(Normal);
+    float diff = max(dot(norm, -lightDir), 0.0);
+    vec3 diffuse = lightColor * (diff * texture(texture_diffuse1, TexCoords).rgb);
 
-    float diffuse = max(dot(norm, -lightDir), 0.0);
+    // specular
+    vec3 viewDir = normalize(viewPos - FragPos);
+    vec3 reflectDir = reflect(lightDir, norm);
+    float spec = pow(max(dot(viewDir, reflectDir), 0.0), materialShininess);
+    vec3 specular = lightColor * (spec * materialSpecular);
 
-    vec3 texColor = texture(texture_diffuse1, TexCoords).rgb;
+    vec3 result = ambient + diffuse + specular;
 
-    FragColor = vec4(texColor * diffuse, 1.0);
+    FragColor = vec4(result, 1.0);
 }
--- app/src/MainController.cpp
+++ app/src/MainController.cpp
@@ -52,11 +52,23 @@ void app::MainController::draw_boat() {
     auto resources = engine::core::Controller::get<engine::resources::ResourcesController>();
     auto boat = resources->model("boat");
     auto shader = resources->shader("basic");
+
     shader->use();
-    glm::vec3 lightDir(-0.3f, -0.9f, -0.95f);
+    glm::vec3 lightDir = glm::normalize(glm::vec3(-0.3f, -0.9f, -0.95f));
+
+    glm::vec3 ambient = glm::vec3(0.15f);
+    glm::vec3 specular = glm::vec3(0.2f, 0.2f, 0.2f);
+    float shininess = 20.0f;
+
     shader->set_vec3("lightDir", lightDir);
+    shader->set_vec3("materialAmbient", ambient);
+    shader->set_vec3("materialSpecular", specular);
+    shader->set_float("materialShininess", shininess);
+
     shader->set_mat4("projection", graphics->projection_matrix());
     shader->set_mat4("view", graphics->camera()->view_matrix());
+    shader->set_vec3("viewPos", graphics->camera()->Position);
+
     glm::mat4 model = glm::mat4(1.0f);
     model = translate(model, glm::vec3(0.0f, 0.0f, -3.0f));
     model = scale(model, glm::vec3(0.3));
