01732ddfc6bf4048256a9d85b4857f7090fe209e
Tadija <tadijamatin0708@gmail.com>
2026-08-07 14:27:07 +0200

    Add blum suport to existing shaders

--- app/resources/shaders/basic.glsl
+++ app/resources/shaders/basic.glsl
@@ -23,7 +23,8 @@ void main()
 //#shader fragment
 #version 330 core
 
-out vec4 FragColor;
+layout(location = 0) out vec4 FragColor;
+layout(location = 1) out vec4 BrightColor;
 
 in vec2 TexCoords;
 in vec3 Normal;
@@ -215,4 +216,11 @@ void main() {
         result += calculateSpotLight();
 
     FragColor = vec4(result, 1.0);
+
+    float brightness = dot(result, vec3(0.2126, 1.0f, 0.0722));
+
+    if (brightness > 1.0)
+        BrightColor = vec4(result, 1.0);
+    else
+        BrightColor = vec4(0.0, 0.0, 0.0, 1.0);
 }
--- app/resources/shaders/skybox.glsl
+++ app/resources/shaders/skybox.glsl
@@ -16,7 +16,9 @@ void main()
 
 //#shader fragment
 #version 330 core
-out vec4 FragColor;
+
+layout(location = 0) out vec4 FragColor;
+layout(location = 1) out vec4 BrightColor;
 
 in vec3 TexCoords;
 
@@ -24,5 +26,13 @@ uniform samplerCube skybox;
 
 void main()
 {
-    FragColor = texture(skybox, TexCoords);
+    vec3 result = texture(skybox, TexCoords).rgb;
+    FragColor = vec4(result, 1.0f);
+
+    float brightness = dot(result, vec3(0.2126, 1.0, 0.0722));
+
+    if (brightness > 1.0)
+        BrightColor = vec4(result, 1.0);
+    else
+        BrightColor = vec4(0.0, 0.0, 0.0, 1.0);
 }
